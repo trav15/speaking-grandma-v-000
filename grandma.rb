fb562ee2a2fcadd73608c58d99b727393f95c502
Trav15 <travistokuyama@gmail.com>
2018-09-12 13:48:03 -0400

    Fix incomplete solution
    Added elsif to include "I LOVE YOU GRANDMA!" condition

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,8 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
   end
